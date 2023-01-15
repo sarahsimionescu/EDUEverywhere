8f8c42dff565184c1e01a3818ffec1e717d42ff4
Selina <selinakim343@outlook.com>
2023-01-14 22:07:37 -0500

    Setup create react app for frontend

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <div className="Main-logo" alt="EDU Everywhere logo">
+          </div>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar;
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,27 @@
+import logo from '../logo.svg';
+import Navbar from '../components/Navbar.js'
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <img src={logo} className="Home-logo" alt="logo" />
+        <p>
+          Edit <code>src/Home.js</code> and save to reload.
+        </p>
+        <a
+          className="Home-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Home;
